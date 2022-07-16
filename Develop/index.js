59ebeca283dff01200b0cb4c13f3d2f23eca6ff1
Kelly Wacker <kelwacker@gmail.com>
2022-07-15 22:31:34 -0400

    stuff

--- Develop/index.js
+++ Develop/index.js
@@ -1,19 +1,86 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else { 
+              console.log('Please enter your name!');
+              return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'username',
-        message: 'What is your username?'
+        message: 'What is your username?',
+        validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub username!');
+              return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'project name',
+        message: 'What is your project name?',
+        validate: projName => {
+            if (projName) {
+              return true;
+            } else {
+              console.log('Please enter your Project Name!');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => confirmAbout
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
     }
 ];
 
@@ -27,4 +94,13 @@ function init() {
 }
 
 // Function call to initialize app
-init();
+init()
+    .then(writeToFile)
+    .then(data => {
+        const pageHTML = generateMarkdown(data);
+
+        fs.writeFile('./README.md', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log("Read Me created. Check out README.md to see it.")
+        })
+    })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `
+    ### License
+    Link: ${license}
+  `;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -17,4 +30,11 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown => {
+  const { badge, link, ...rest} = generateMarkdown;
+  return `
+  ## License
+  ${renderLicenseBadge}
+  ${renderLicenseLink}
+  `;
+};
